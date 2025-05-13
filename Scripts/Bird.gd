54563cc82e8829c626b612a1e34ed64073b48bb4
Ganaini <mahmoud.abdallah09@gmail.com>
2025-05-13 19:51:47 +0900

    Added sounds

--- Scripts/Bird.gd
+++ Scripts/Bird.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 @export var flap_strength: float = -300.0
 @export var gravity: float = 800.0
 @onready var anim = $AnimatedSprite2D
+@onready var flap_sound = $FlapSound
+@onready var score_sound = $ScoreSound
+@onready var death_sound = $DeathSound
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = flap_strength
+		flap_sound.play()
 
 	velocity.y += gravity * delta
 	move_and_slide()
@@ -19,9 +23,10 @@ func _ready():
 
 
 func _on_area_2d_hitbox_area_entered(area: Area2D) -> void:
-	print("Collided with: ", area.name, " | Groups: ", area.get_groups())
 	if area.is_in_group("Pipe"):
+		death_sound.play()
 		get_node("/root/Main").show_game_over()
 	elif area.is_in_group("ScoreZone"):
+		score_sound.play()
 		get_node("/root/Main").add_score()
 		area.queue_free()  # prevent double scoring
